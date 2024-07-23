27e2824e4896fb277e0ece6c9d3fdccbf8a9bf45
yangggu <sh_yang90@naver.com>
2024-07-23 14:45:14 +0900

    app.py에 호스트0.0.0.0추가

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def index():
 atexit.register(WebDriverSingleton.quit_instance)
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5500)
+    app.run(debug=False, host='0.0.0.0', port=5500)
