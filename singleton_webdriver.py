557b23ac79b700f635028c99d5268612a5861f7b
yangggu <sh_yang90@naver.com>
2024-07-24 23:21:32 +0900

    path try 3

--- singleton_webdriver.py
+++ singleton_webdriver.py
@@ -1,10 +1,6 @@
-# import os
-# os.environ['PATH'] = os.environ.get('PATH', '') + ':/path/to/chrome'
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import WebDriverException
-from webdriver_manager.chrome import ChromeDriverManager
 
 class WebDriverSingleton:
     _instance = None
@@ -13,14 +9,14 @@ class WebDriverSingleton:
     def get_instance(cls):
         if cls._instance is None:
             try:
-                service = Service(ChromeDriverManager().install())
+                service = Service()
                 options = webdriver.ChromeOptions()
                 options.add_argument('--headless')
                 options.add_argument('--no-sandbox')
                 options.add_argument('--disable-dev-shm-usage')
                 options.add_argument('--disable-gpu') 
                 options.add_argument('--window-size=1920x1080') 
-                cls._instance = webdriver.Chrome(service=service, options=options, executable_path='/path/to/chromedriver')
+                cls._instance = webdriver.Chrome(service=service, options=options)
             except WebDriverException as e:
                 print(f"웹 드라이버 오류: {e}")
         return cls._instance
