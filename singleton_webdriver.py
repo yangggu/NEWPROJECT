004eaab30a87cd8716897b1e6d4c6de30880898e
yangggu <sh_yang90@naver.com>
2024-07-24 22:47:09 +0900

    chromedriver version rollback, add exception, headless mode off

--- singleton_webdriver.py
+++ singleton_webdriver.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
 from webdriver_manager.chrome import ChromeDriverManager
 
 class WebDriverSingleton:
@@ -8,14 +9,17 @@ class WebDriverSingleton:
     @classmethod
     def get_instance(cls):
         if cls._instance is None:
-            service = Service(ChromeDriverManager(version='114.0.5735.90').install())
-            options = webdriver.ChromeOptions()
-            options.add_argument('--headless')
-            options.add_argument('--no-sandbox')
-            options.add_argument('--disable-dev-shm-usage')
-            options.add_argument('--disable-gpu') 
-            options.add_argument('--window-size=1920x1080') 
-            cls._instance = webdriver.Chrome(service=service, options=options)
+            try:
+                service = Service(ChromeDriverManager().install())
+                options = webdriver.ChromeOptions()
+                # options.add_argument('--headless')
+                options.add_argument('--no-sandbox')
+                options.add_argument('--disable-dev-shm-usage')
+                options.add_argument('--disable-gpu') 
+                options.add_argument('--window-size=1920x1080') 
+                cls._instance = webdriver.Chrome(service=service, options=options)
+            except WebDriverException as e:
+                print(f"웹 드라이버 오류: {e}")
         return cls._instance
 
     @classmethod
