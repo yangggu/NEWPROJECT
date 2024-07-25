e570d4c463c97f78e7dcb7a21f52e5d311b8da06
yangggu <sh_yang90@naver.com>
2024-07-25 13:26:17 +0900

    singleton webdriver to initial commit

--- singleton_webdriver.py
+++ singleton_webdriver.py
@@ -8,26 +8,18 @@ class WebDriverSingleton:
     @classmethod
     def get_instance(cls):
         if cls._instance is None:
-            try:
-                service = Service(ChromeDriverManager().install())
-                options = webdriver.ChromeOptions()
-                options.add_argument('--headless')
-                options.add_argument('--no-sandbox')
-                options.add_argument('--disable-dev-shm-usage')
-                options.add_argument('--disable-gpu') 
-                options.add_argument('--window-size=1920x1080') 
-                cls._instance = webdriver.Chrome(service=service, options=options)
-            except Exception as e:
-                print(f"Failed to create WebDriver instance: {e}")
-                cls._instance = None
+            service = Service(ChromeDriverManager().install())
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            options.add_argument('--disable-gpu') 
+            options.add_argument('--window-size=1920x1080') 
+            cls._instance = webdriver.Chrome(service=service, options=options)
         return cls._instance
 
     @classmethod
     def quit_instance(cls):
         if cls._instance is not None:
-            try:
-                cls._instance.quit()
-            except Exception as e:
-                print(f"Failed to quit WebDriver instance: {e}")
-            finally:
-                cls._instance = None
+            cls._instance.quit()
+            cls._instance = None
